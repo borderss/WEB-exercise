280423c8d08b1e3eff88eac59068b1c0e4c23337
Alberts Liepins <liepins01@gmail.com>
2022-10-09 02:08:51 +0300

    Dynamic, styalized scrollbar

---
+++ script.js
@@ -0,0 +1,13 @@
+// Scrollbar handling
+var isScrolling
+
+window.addEventListener("scroll", (e) => {
+  if (!document.body.classList.contains("scroll-active")) {
+    document.body.classList.toggle("scroll-active")
+  }
+
+  clearTimeout(isScrolling)
+  isScrolling = setTimeout(() => {
+    document.body.classList.toggle("scroll-active")
+  }, 1000)
+})
