80d4e2296c34447be84235389f635fb9dbc7a903
Alberts Liepins <liepins01@gmail.com>
2022-11-06 04:14:32 +0200

    formatting

--- public/script.js
+++ public/script.js
@@ -23,17 +23,23 @@ let quotePerspectiveHandler = (e) => {
   let degX = Math.max(-10, Math.min(10, (x - quote.clientWidth / 2) / 50))
   let degY = -Math.max(-10, Math.min(10, (y - quote.clientHeight / 2) / 20))
 
-  quotecontainer.setAttribute("style", `transform: perspective(500px) rotateX(${degY}deg) rotateY(${degX}deg);`)
+  quotecontainer.setAttribute(
+    "style",
+    `transform: perspective(500px) rotateX(${degY}deg) rotateY(${degX}deg);`
+  )
 }
 
-let moveListener = quote.addEventListener('mousemove', quotePerspectiveHandler)
+let moveListener = quote.addEventListener("mousemove", quotePerspectiveHandler)
 
-quote.addEventListener('mouseleave', ()=>{
-  quote.removeEventListener('mousemove', quotePerspectiveHandler)
+quote.addEventListener("mouseleave", () => {
+  quote.removeEventListener("mousemove", quotePerspectiveHandler)
 
-  quotecontainer.setAttribute("style", `transform: perspective(500px) rotateX(0deg) rotateY(0deg); transition: 0.5s ease-out;  `)
-  setTimeout(()=>{
-    quote.addEventListener('mousemove', quotePerspectiveHandler)
+  quotecontainer.setAttribute(
+    "style",
+    `transform: perspective(500px) rotateX(0deg) rotateY(0deg); transition: 0.5s ease-out;  `
+  )
+  setTimeout(() => {
+    quote.addEventListener("mousemove", quotePerspectiveHandler)
   }, 500)
 })
 
@@ -41,21 +47,22 @@ document.getElementById("disc").addEventListener("click", () => {
   navigator.clipboard.writeText("SnowyBall#0001")
 
   let span = document.getElementById("disc").getElementsByTagName("span")[0]
-  
+
   span.innerHTML = "Copied!"
   span.style.color = "#0c2"
   setTimeout(() => {
     span.innerHTML = "SnowyBall#0001"
-    span.style.color = getComputedStyle(document.documentElement).getPropertyValue("--text")
+    span.style.color = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue("--text")
     span.style.transition = "0.15s"
 
     span.removeAttribute("style")
-  }, 500);
+  }, 500)
 })
 
 document.getElementById("email").addEventListener("click", () => {
   console.log("DAUNIS")
-
 })
 
 document.getElementById("email").addEventListener("click", () => {
@@ -63,49 +70,62 @@ document.getElementById("email").addEventListener("click", () => {
   window.location = "mailto: liepins01@gmail.com"
 
   let span = document.getElementById("email").getElementsByTagName("span")[0]
-  
+
   span.innerHTML = "Redirecting!"
   span.style.color = "#0c2"
   setTimeout(() => {
     span.innerHTML = "liepins01@gmail.com"
-    span.style.color = getComputedStyle(document.documentElement).getPropertyValue("--text")
+    span.style.color = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue("--text")
     span.style.transition = "0.15s"
-    
+
     span.removeAttribute("style")
-  }, 500);
+  }, 500)
 })
 
 var observer = new IntersectionObserver((entries) => {
   console.log(entries)
-  entries.forEach(e => {
-    if(e.isIntersecting === true){
-      e.target.setAttribute("style", `transition-delay: ${e.target.getAttribute("stagger")} !important; opacity: 1 !important; transform: translateY(0px) !important; transition: opacity 0.75s, transform 0.75s;`)
+  entries.forEach((e) => {
+    if (e.isIntersecting === true) {
+      e.target.setAttribute(
+        "style",
+        `transition-delay: ${e.target.getAttribute(
+          "stagger"
+        )} !important; opacity: 1 !important; transform: translateY(0px) !important; transition: opacity 0.75s, transform 0.75s;`
+      )
       setTimeout(() => {
         e.target.classList.remove("float-in")
         setTimeout(() => {
           e.target.removeAttribute("style")
-        }, 3000);
-      }, 750);
+        }, 3000)
+      }, 750)
     }
   })
-  
 })
 
-Array.from(document.querySelectorAll(".float-in")).forEach(e => {
+Array.from(document.querySelectorAll(".float-in")).forEach((e) => {
   observer.observe(e)
 })
 
 function clearNavbar() {
-  Array.from(document.querySelector(".navbar").getElementsByTagName("a")).forEach((e)=>{
+  Array.from(
+    document.querySelector(".navbar").getElementsByTagName("a")
+  ).forEach((e) => {
     e.classList.remove("active-nav-item")
   })
 }
 
-Array.from(document.querySelector(".navbar").getElementsByTagName("a")).forEach((e)=>{
-  e.addEventListener("click", (_)=>{
-    if(!e.classList.contains("active-nav-item") && !e.classList.contains("info")){
-      clearNavbar()
-      e.classList.add("active-nav-item")
-    }
-  })
-})
+Array.from(document.querySelector(".navbar").getElementsByTagName("a")).forEach(
+  (e) => {
+    e.addEventListener("click", (_) => {
+      if (
+        !e.classList.contains("active-nav-item") &&
+        !e.classList.contains("info")
+      ) {
+        clearNavbar()
+        e.classList.add("active-nav-item")
+      }
+    })
+  }
+)
