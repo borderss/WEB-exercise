7040898166aebb0ade1ce882d6caf687e169a043
Alberts Liepins <liepins01@gmail.com>
2022-11-26 20:23:46 +0200

    DB Seeder created, default user seeding, groundwork for messages created

--- api/portfolio/database/seeders/DatabaseSeeder.php
+++ api/portfolio/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+// use App\Models\Messages;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,6 +23,33 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
+        // ]);
+
+        // USERS
+        User::create([
+          'name' => 'user1',
+          'email' => 'user1@gmail.com',
+          'password' => Hash::make('password1'),
+        ]);
+        User::create([
+          'name' => 'user2',
+          'email' => 'user2@gmail.com',
+          'password' => Hash::make('password2'),
+        ]);
+        User::create([
+          'name' => 'user3',
+          'email' => 'user3@gmail.com',
+          'password' => Hash::make('password3'),
+        ]);
+
+
+        // MESSAGES
+        // Messages::create([
+        //   'user_id' => 1,
+        //   'board_id' => 1,
+        //   'title' => 'Twilio integration',
+        //   'desc' => 'Finally implementing Twilio into the application workflow',
+        //   'color' => '#BADA55'
         // ]);
     }
 }
