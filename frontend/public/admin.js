3e0a7ae9e1dafa3f85e5d3939c5658c5a9bab4f4
[Students] Alberts Liepins <03DPALiepins@rvt.lv>
2022-12-12 13:22:23 +0200

    fake data for admin panel

---
+++ frontend/public/admin.js
@@ -0,0 +1,45 @@
+import fakeInquiryData from './fakeData.json' assert { type: "json"}
+
+let tb = document.getElementById("tableBody")
+
+for (let i = 0; i < fakeInquiryData.length; i++) {
+  let tr = document.createElement("tr")
+  let subject = document.createElement("td")
+  let fullName = document.createElement("td")
+  let email = document.createElement("td")
+  let content = document.createElement("td")
+
+  let contentDiv = document.createElement("div")
+  content.appendChild(contentDiv)
+  
+  if (fakeInquiryData[i].jobRelated) {
+    subject.classList.add("subject") 
+    subject.setAttribute("type", "job-related")
+  } else {
+    subject.classList.add("subject")
+  }
+  fullName.classList.add("full-name")
+  email.classList.add("email")
+  content.classList.add("content")
+  
+  // <tr>
+  //   <td class="subject" type="job-related">Possible job offer</td>
+  //   <td class="full-name">Zigfrīdmans<br />Austenbrooks</td>
+  //   <td class="email">example.email@gmail.com</td>
+  //   <td class="content"><div>ipsum dolor sit amet consectetur adipisicing elit. Recusandae blanditiis autem
+  //     aliquam ad quo ipsum itaque est quidem eligendi earum, dolor id? Aliquam iste at unde omnis nulla quia
+  //     totam!</div></td>
+  // </tr>
+
+  subject.innerText = fakeInquiryData[i].subject
+  fullName.innerHTML = fakeInquiryData[i].fullName
+  email.innerText = fakeInquiryData[i].email
+  contentDiv.innerHTML = fakeInquiryData[i].message
+
+  tr.appendChild(subject)
+  tr.appendChild(fullName)
+  tr.appendChild(email)
+  tr.appendChild(content)
+
+  tb.appendChild(tr)
+}
